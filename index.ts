ec1ad44af072c54d1920741c6d9786dffb6b77f5
kartik-dhawan <kartikkdhawan@gmail.com>
2024-02-23 15:50:22 +0530

    integrated express with graphql

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import { ApolloServer } from "@apollo/server"
-import { startStandaloneServer } from "@apollo/server/standalone"
+import { expressMiddleware } from "@apollo/server/express4"
 import { typeDefs } from "./src/schema.ts"
 import { resolvers } from "./resolvers.ts"
-// import { readFileSync } from "fs"
+import express from "express"
+import cors from "cors"
 
-// const TYPE_DEFS = readFileSync("./schema.graphql", { encoding: "utf-8" })
+const app = express()
 
 const server = new ApolloServer({
   // data schema - map of data structure
@@ -14,10 +15,16 @@ const server = new ApolloServer({
   resolvers,
 })
 
-const { url } = await startStandaloneServer(server, {
-  listen: {
-    port: 4000,
-  },
+await server.start()
+
+app.use(cors())
+app.use(express.json())
+app.use("/graphql", expressMiddleware(server))
+app.get("/", (req, res) => {
+  res.json({ name: "kartik" })
 })
 
-console.log("Server ready at url: ", url)
+app.listen(4000, () => {
+  console.log("Server ready at url: https://localhost:4000")
+  console.log("GraphQL playground ready at url: https://localhost:4000/graphql")
+})
